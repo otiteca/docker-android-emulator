54623f9ec5825c1fa2959ef02f8a6e1331685ecd
Olivier Titéca-Beauport <olivier@hub-scan.com>
2017-05-22 13:17:40 +0200

    change softsam by otiteca and update he start.sh

--- android-base/start.sh
+++ android-base/start.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+vncdisplay=$VNCDISPLAY
+if [ -z "$vncdisplay" ]
+then
+    vncdisplay=":0"
+fi
+
+gpuon=$GPUON
+if [ -z "$gpuon" ]
+then
+    gpuon="off"
+fi
+
+
 android_arch=$ANDROID_ARCH
 if [ -z "$android_arch" ]
 then
@@ -10,4 +23,8 @@ ip=$(ip addr list eth0|grep "inet "|cut -d' ' -f6|cut -d/ -f1)
 redir --laddr=$ip --lport=5555 --caddr=127.0.0.1 --cport=5555 &
 
 # Set up and run emulator
-emulator64-${android_arch} -avd ${android_arch} -http-proxy "$http_proxy" -noaudio -no-window -gpu off -verbose -qemu -vnc :0
+emulator64-${android_arch} -avd ${android_arch} -http-proxy "$http_proxy" -noaudio -no-window -gpu ${gpuon} -verbose -qemu -vnc ${vncdisplay}
+
+
+
+
